585c2a517ce9c9555cf5eeb3586865e38925974e
bismaputraatallah <148869071+bismaputraatallah@users.noreply.github.com>
2023-11-22 21:18:46 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function showSlides(n) {
     if (n > slides.length) { slideIndex = 1}
     if (n > 1) { slideIndex = slides.length }
     for ( i = 0; i < slides.length; i++) {
-        slides[i].stye.displaysplay = "none";
+        slides[i].stye.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active","")

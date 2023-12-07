cb251fd09efc75eb9259e30a88fdd341d836e0f3
pranandaindraa <100947545+pranandaindraa@users.noreply.github.com>
2023-12-07 19:10:18 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,5 +25,51 @@ var slideIndex = 1;
             dots[slideIndex - 1].className += " active";
         }
 
+// news api 
+
+API_KEY = "61e80d9448f346da9b4717ba608bf1d7"
+const url = "https://newsapi.org/v2/everything?q=apple&from=2023-11-30&to=2023-11-30&sortBy=popularity="
+// const url = "https://newsapi.org/v2/top-headlines?country="
+
+async function fetchData(query){
+
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
+    // console.log(`$(url)$(query)&apiKey=${API_KEY}`)
+    const data = await res.json()
+    console.log(data)
+    return data
+}
+fetchData("all").then(data => renderMain(data.articles))
+
+
+
+//render news 
+function renderMain(arr){
+    let mainHTML = ''
+    for(let i = 0 ; i < arr.length ;i++){
+        if(arr[i].urlToImage){
+        mainHTML += ` <div class="card">
+                        <a href=${arr[i].url}>
+                        <img src=${arr[i].urlToImage} lazy="loading" />
+                        <h4>${arr[i].title}</h4>
+                        <div class="publishbyDate">
+                            <p>${arr[i].source.name}</p>
+                            <span>•</span>
+                            <p>${new Date(arr[i].publishedAt).toLocaleDateString()}</p>
+                        </div>
+                        <div class="desc">
+                           ${arr[i].description}
+                        </div>
+                        </a>
+                     </div>
+        `
+        }
+    }
+
+    document.querySelector("main").innerHTML = mainHTML
+}
+
+
+
 
 

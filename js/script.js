2ef00af4c984f1bdfb30b65e63e33c7c30564e98
pranandaindraa <100947545+pranandaindraa@users.noreply.github.com>
2023-12-11 12:04:35 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,47 +27,56 @@ var slideIndex = 1;
 
 // news api 
 
-API_KEY = "61e80d9448f346da9b4717ba608bf1d7"
-const url = "https://newsapi.org/v2/everything?q=apple&from=2023-11-30&to=2023-11-30&sortBy=popularity="
-// const url = "https://newsapi.org/v2/top-headlines?country="
+async function fetchNews() {
+    const apiKey = "61e80d9448f346da9b4717ba608bf1d7"; //masukan api key pribadi
+    const apiUrl = `https://newsapi.org/v2/everything?q=apple&from=2023-11-30&to=2023-11-30&sortBy=popularity=us&apiKey=${apiKey}`;
 
-async function fetchData(query){
+    try {
+        
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
-    // console.log(`$(url)$(query)&apiKey=${API_KEY}`)
-    const data = await res.json()
-    console.log(data)
-    return data
-}
-fetchData("all").then(data => renderMain(data.articles))
-
-
-
-//render news 
-function renderMain(arr){
-    let mainHTML = ''
-    for(let i = 0 ; i < arr.length ;i++){
-        if(arr[i].urlToImage){
-        mainHTML += ` <div class="card">
-                        <a href=${arr[i].url}>
-                        <img src=${arr[i].urlToImage} lazy="loading" />
-                        <h4>${arr[i].title}</h4>
-                        <div class="publishbyDate">
-                            <p>${arr[i].source.name}</p>
-                            <span>•</span>
-                            <p>${new Date(arr[i].publishedAt).toLocaleDateString()}</p>
-                        </div>
-                        <div class="desc">
-                           ${arr[i].description}
-                        </div>
-                        </a>
-                     </div>
-        `
+        
+        if (data.status === 'ok') {  
+            return data.articles;
+        } else {
+            console.error('Error fetching news:', data.message);
+            return [];
         }
+    } catch (error) {
+        console.error('Error fetching news:', error);
+        return [];
+    }
+}
+
+// Function menampilkan artikel
+async function displayNews() {
+    const mainContent = document.getElementById('main-content');
+    const headlines = document.getElementById('headlines');
+
+
+    const newsData = await fetchNews();
+    mainContent.innerHTML = '';
+
+    if (newsData.length === 0) {
+        mainContent.innerHTML = '<p>No news available at the moment.</p>';
+        return;
     }
 
-    document.querySelector("main").innerHTML = mainHTML
+    newsData.forEach((articleData, index) => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <img class='imageArtikel'>${article.urlToImage}</img>
+            <h2>${articleData.title}</h2>
+            <p><strong>${articleData.source.name}</strong></p>
+            <p>${articleData.description}</p>
+            <a href="artikel.html?article=${index}">Read more</a>
+        `;
+        mainContent.appendChild(article);
+    });
 }
+
+window.onload = displayNews;
 
 
 
